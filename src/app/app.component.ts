ce41ba24b7cbef58926fc4455a83e5d69375ff92
Amit Borkar <amitg.borkar@gmail.com>
2024-11-17 09:54:51 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataBindingComponent],
   templateUrl: './app.component.html',
+  // template: `<h1>Hello World</h1>`,
   styleUrl: './app.component.css'
+  // styles: `h1{color:red;}`
 })
 export class AppComponent {
   title = 'first-app';
